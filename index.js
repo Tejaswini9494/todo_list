36c4ad9ffd03f71ffa05bf224e6d8805d25affef
Tejaswini9494 <47525219+Tejaswini9494@users.noreply.github.com>
2020-02-21 20:47:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+const express= require("express");
+const bodyParser = require("body-parser");
+
+//Connection Part
+const mongoose = require("mongoose");
+
+var url ="";
+mongoose.connect("mongodb+srv://Tejaswini:tejaswini@cluster0-b6olv.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false});
+/*
+(err) => {
+    if(!err) { console.log('Connection Successful')}
+    else { console.log('Error in DB connection:' + err)}
+}; */
+
+const app=express();
+const TodoTask = require("./models/TodoTask");
+app.use("/static", express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// GET METHOD
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todo.ejs", { todoTasks: tasks });
+    });
+});
+
+//POST METHOD
+app.post('/', async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
+});
+
+//UPDATE
+app.route("/edit/:id").get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    }).post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
+
+var port= process.env.PORT || 8080;
+app.listen(port,()=> { 
+    console.log("Entered yaarrr");
+    console.log("Server Up & Running: "+ port);
+})
